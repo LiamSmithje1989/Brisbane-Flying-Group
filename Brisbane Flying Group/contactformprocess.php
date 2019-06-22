eb76414afdb31cec493bf230ada209873b4d8dac
LiamSmithje1989 <51841140+LiamSmithje1989@users.noreply.github.com>
2019-06-22 13:39:29 +0200

    Update contactformprocess.php

--- Brisbane Flying Group/contactformprocess.php
+++ Brisbane Flying Group/contactformprocess.php
@@ -12,14 +12,5 @@
 		mail($to, $subject, $message, $headers);
 		header("Location: membership-enquiries.html");
 	}
-
-
-	/*$to      = '//';
-	$subject = 'the subject';
-	$message = 'hello';
-	$headers = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-	*/
 	
 ?>
