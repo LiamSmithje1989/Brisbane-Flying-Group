d8f8126595af58772618733caf3df426898f0cbb
LiamSmithje1989 <51841140+LiamSmithje1989@users.noreply.github.com>
2019-06-22 13:38:29 +0200

    Create contactformprocess.php

---
+++ Brisbane Flying Group/contactformprocess.php
@@ -0,0 +1,25 @@
+<?php
+	$to = ' inquiry@brisflying.org';
+	$subject = 'the subject';
+	$message = 'From: ' . $_POST['email'] . "\r\n" . 'From: ' . $_POST['name'] . "\r\n\r\n" . $_POST['message'];
+	$headers = 'From: ' . $_POST['email'] . "\r\n" .
+    'Name: ' . $_POST['name'];
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+	if($name != "" || $name != null || $email != "" || $email != null || $message != "" || $email != null) {
+		mail($to, $subject, $message, $headers);
+		header("Location: membership-enquiries.html");
+	}
+
+
+	/*$to      = 'coliga1@coliga.nl';
+	$subject = 'the subject';
+	$message = 'hello';
+	$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+	*/
+	
+?>
